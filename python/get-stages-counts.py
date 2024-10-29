9983324b188c2e96796371ceb1ba99bc4e2117b8
daveh <dave.hunsinger@gmail.com>
2024-10-28 21:25:48 -0700

    first commit

--- python/get-stages-counts.py
+++ python/get-stages-counts.py
@@ -1,3 +1,27 @@
+#!/usr/bin/env python
+
+'''
+This script generates a list of all stages used in pipelines used in jobs on Legacy Control
+Hub (SCH 3.x). Script looks at the last job run for pipeline definitions.
+
+Prerequisites:
+ - Python 3.6+; Python 3.9+ preferred
+
+ - StreamSets SDK for Python v3.x
+   See: https://docs.streamsets.com/sdk/latest/index.html
+
+ - Legacy Control Hub username/password for a user with Organization Administrator role
+
+ - To avoid including User Credentials in the script, export these two environment variables
+   prior to running the script:
+
+        export USER_ID=<your user id>>
+        export PASS=<your password>
+ - Optional: Filter jobs based on data collector labels. Uncomment lines 37, 38, 57, comment out
+   line 58.
+
+'''
+
 from streamsets.sdk import ControlHub
 import os,json
 
@@ -12,7 +36,7 @@ PASS = os.getenv('PASS')
 SCH_URL = 'https://trailer.streamsetscloud.com'
 
 # LABEL for jobs to target
-#LABEL = 'natwest'
+#LABEL = 'foo'
 
 #Connect to Control Hub
 sch = ControlHub(
